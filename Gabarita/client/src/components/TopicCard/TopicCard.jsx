585c45a17b74ac1246db0eab64a7b186f4a192f8
Tito <joaogtito@gmail.com>
2025-11-17 14:19:28 -0300

    socorro

---
+++ Gabarita/client/src/components/TopicCard/TopicCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './TopicCard.css';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+const TopicCard = ({ post }) => {
+  // Adicionar verificação para evitar o erro "post is undefined"
+  if (!post) return null; 
+
+    const formattedDate = format(new Date(post.createdAt), "dd 'de' MMMM 'de' yyyy", { locale: ptBR });
+
+  return (
+    <Link to={`/post/${post.id}`} className="topic-card-link">
+      <div className="topic-card">
+        <div className="topic-card-header">
+          <span className="topic-card-subject">{post.subject}</span>
+        </div>
+        <h3 className="topic-card-title">{post.title}</h3>
+        <div className="topic-card-footer">
+          <span className="topic-card-author">Por: {post.author.name}</span>
+          <span className="topic-card-date-footer"> • {formattedDate}</span>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default TopicCard;
